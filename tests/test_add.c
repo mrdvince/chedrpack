07323f9d6d91eba608f813ef8b03dde87c025d59
Vince <vincivenv@gmail.com>
2023-10-22 14:55:08 +0200

    Basic test

---
+++ tests/test_add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "../include/rustrpm.h"
+
+int test_add() {
+    int32_t result = add(10, 20);
+    if (result == 30) {
+        printf("test_add passed\n");
+        return 0;
+    } else {
+        printf("test_add failed\n");
+        return 1;
+    }
+}
+
+int main() {
+    int res_code = test_add();
+    return res_code;
+}
